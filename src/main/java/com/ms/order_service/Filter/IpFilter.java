67634b95bd9ce31f9249abbe02220da10efcdbb3
sourav-18 <souravhd2@gmail.com>
2026-04-22 08:28:59 +0530

    dd

---
+++ src/main/java/com/ms/order_service/Filter/IpFilter.java
@@ -0,0 +1,32 @@
+package com.ms.order_service.Filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+@Component
+@Profile("prod")
+public class IpFilter extends OncePerRequestFilter {
+
+    @Value("${server.allow.id}")
+    private String ALLOW_IP;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String[] ips = ALLOW_IP.split(",");
+        String clientIp = request.getRemoteAddr();
+        if (Arrays.stream(ips).noneMatch(item->item.equals(clientIp))) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Forbidden");
+            return;
+        }
+        filterChain.doFilter(request,response);
+    }
+}
